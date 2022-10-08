71a2a5bfb9460b1f7ff512f2e9b35aa812089943
zongyanggong <115329613+zongyanggong@users.noreply.github.com>
2022-10-08 08:50:03 -0400

    change + to -

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def print_hi(name):
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 def my_function(a, b):
-    return a+b
+    return a-b
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
