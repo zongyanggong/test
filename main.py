0309537a88d05c91e83bb598f6d15bdd2b568158
zongyanggong <zongyanggong@gmail.com>
2022-10-08 08:41:16 -0400

    test new function

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hello, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
+def my_function(a, b):
+    return a+b
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
@@ -15,5 +17,7 @@ if __name__ == '__main__':
     print('How are you')
     print('Where are you from?')
     print(password)
+
+    print(my_function(2, 4))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
